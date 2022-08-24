0da0def3a9f7a336c5e6e3342bb46bd590fd258d
Vitor Wagner <vitormwagner@gmail.com>
2022-08-24 16:56:22 -0300

    Desafio 11

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,41 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+// Para atender a todos os requisitos do desafio foi necessário quebrar a funcionalidade em várias funções auxiliares.
+function checkInvalidNumber(numberArray) {
+  for (let number of numberArray) {
+    if (number < 0 || number > 9) {
+      return true;
+    }
+  }
+  return false;
+}
+
+// Função auxiliar para fazer a contagem dos elementos no array. Referência: https://stackoverflow.com/questions/19395257/how-to-count-duplicate-value-in-an-array-in-javascript
+function numberCount(numberArray) {
+  let count = {};
+  numberArray.forEach(function (i) { count[i] = (count[i] || 0) + 1; });
+  return count;
+}
+
+function countCheck(count) {
+  let countObject = numberCount(count);
+  for (let key in countObject) {
+    if (countObject[key] >= 3) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function generatePhoneNumber(numberArray) {
+  if (numberArray.length !== 11) {
+    return 'Array com tamanho incorreto.';
+  }
+  if (checkInvalidNumber(numberArray) === true || countCheck(numberArray) === true) {
+    return 'não é possível gerar um número de telefone com esses valores';
+  }
+  let ddd = `(${numberArray.splice(0, 2).join('')})`;
+  let phoneNumber = `${ddd} ${numberArray.splice(0, 5).join('')}-${numberArray.join('')}`;
+  return phoneNumber;
 }
 
 // Desafio 12
